d4f8ce78b70eb470ea97943bec38af3f14297ab2
ebukadev <degwu2020@gmail.com>
2023-02-20 15:42:50 +0100

    styling and adding scrolling to the divs

--- src/pages/Jockey.jsx
+++ src/pages/Jockey.jsx
@@ -1,36 +1,87 @@
 import Logo from "../assets/logo.svg";
 import header from "../assets/header.svg";
 import Avater from "../assets/Avater.jfif";
-import stats from "../utils/stats";
+import { stats, recents } from "../utils/stats";
+import { useRef, useState } from "react";
 
 const Jockey = () => {
+  const scrollableDivRef = useRef(null);
+  const [isMouseDown, setIsMouseDown] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [scrollLeft, setScrollLeft] = useState(0);
+
+  function handleMouseDown(e) {
+    setIsMouseDown(true);
+    setStartX(e.pageX - scrollableDivRef.current.offsetLeft);
+    setScrollLeft(scrollableDivRef.current.scrollLeft);
+  }
+
+  function handleMouseUp() {
+    setIsMouseDown(false);
+  }
+
+  function handleMouseMove(e) {
+    if (!isMouseDown) return;
+    e.preventDefault();
+    const x = e.pageX - scrollableDivRef.current.offsetLeft;
+    const walk = (x - startX) * 2;
+    scrollableDivRef.current.scrollLeft = scrollLeft - walk;
+  }
   return (
-    <section className="bg-primary h-screen ">
+    <section className="bg-primary h-screen flex ">
       <aside>
-      <div className="flex">
-        <img src={Logo} alt="Logo" className=" w-16  mt-4 ml-4 " />
-        <img src={header} alt="logo text" className="w-32  mt-4 ml-4" />
-      </div>
-      <div className=" flex flex-row ">
-        <div className="flex flex-col ml-20 ">
-          {stats.map((stat) => (
-            <div key={stat.key}>
-              <div>
-                <h3 className="text-[100px] text-center">{stat.num}</h3>
-                <h5 className="ml-2 text-center">{stat.title}</h5>
-              </div>
-            </div>
-          ))}
+        <div className="flex">
+          <img src={Logo} alt="Logo" className=" w-16  mt-4 ml-4 " />
+          <img src={header} alt="logo text" className="w-32  mt-4 ml-4" />
         </div>
-      </div>
+        <div className=" flex flex-row ">
+          <div className="flex flex-col  ml-20">
+            {stats.map((stat) => (
+              <div key={stat.key}>
+                <div className={stat.key === 1 ? "" : " mt-16"}>
+                  <h3 className="text-[80px] text-center">0{stat.num}</h3>
+                  <h5 className="ml-2 text-center">{stat.title}</h5>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </aside>
-      <div className="h-1/4 w-1/2 -ml-40 bg-dark flex flex-row display: absolute top-16 left-1/3 rounded-3xl">
-        <img src={Avater} alt="image of avater" className="rounded-full  h-32 ml-14 mt-6"/>
-        <h1 className="m-auto  text-white text-4xl display: relative bottom-5 left-16">DJ DEVOID</h1>
-      </div>
-      <div className="">
-        <img src="" alt="" />
-      </div>
+      <section>
+        <div className="  relative w-[700px] top-20 left-16">
+          <div className="flex auto h-[100px] rounded-xl  items-center bg-dark ">
+            <img
+              src={Avater}
+              alt="image of avater"
+              className="rounded-full  h-32 ml-14 mt-10"
+            />
+            <h1 className="m-auto  text-white text-3xl bottom-5 left-16">
+              DJ PRIMATE'S <br /> DASHBOARD
+            </h1>
+          </div>
+          <div
+            className=" h-80 cursor-pointer overflow-hidden "
+            ref={scrollableDivRef}
+            onMouseDown={handleMouseDown}
+            onMouseUp={handleMouseUp}
+            onMouseMove={handleMouseMove}
+          >
+            <div className=" rounded-xl flex justify-between mt-16 w-[140vw] ">
+              {recents.map((recent) => (
+                <div key={recent.key}>
+                  <img
+                    src={recent.img}
+                    className={" rounded-xl w-40 "}
+                    draggable={false}
+                  />
+                  <h2>{recent.song}</h2>
+                  <h5>{recent.song}</h5>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
     </section>
   );
 };
--- src/utils/stats.js
+++ src/utils/stats.js
@@ -1,4 +1,21 @@
-const stats = [
+import img1 from '../assets/img 1.jfif'
+import img2 from '../assets/img 2.jfif'
+
+
+import img3 from '../assets/img 3.jfif'
+import img4 from '../assets/img 4.jfif'
+import img5 from '../assets/img 5.jfif'
+import img6 from '../assets/img 6.jfif'
+import img7 from '../assets/img 7.jfif'
+import img8 from '../assets/img 18.jfif'
+import img9 from '../assets/img 9.jfif'
+import img10 from '../assets/img 10.jfif'
+
+
+
+
+
+  export const stats = [
     {
         num:7,
         title:"listeners",
@@ -17,4 +34,76 @@ const stats = [
 
     }
 ]
-export default stats
+ export const recents = [
+    {
+        img:img1,
+        song:'Night song',
+        artist: 'joseph',
+        key: 1
+    },
+    {
+        img:img2,
+        song:'yoga',
+        artist: 'Asake',
+        key: 2
+
+    },  {
+        img:img3,
+        song:'Buga',
+        artist: 'Kiss Daniel',
+        key: 3
+
+    },  {
+        img:img4,
+        song:'Night walk',
+        artist: 'james',
+        key: 4
+
+    },{
+        img:img5,
+        song:'Belong',
+        artist: 'Ed sheren',
+        key: 5
+
+    },{
+        img:img6,
+        song:'Love',
+        artist: 'gamble',
+        key: 6
+
+    },{
+        img:img7,
+        song:'Heat Waves',
+        artist: 'glass animals',
+        key: 7
+
+    },{
+        img:img8,
+        song:'Night walk',
+        artist: 'james',
+        key: 8
+
+    },{
+        img:img9,
+        song:'Night walk',
+        artist: 'james',
+        key: 9
+
+    },{
+        img:img10,
+        song:'Night walk',
+        artist: 'james',
+        key: 10
+
+    },{
+        img:img2,
+        song:'Night walk',
+        artist: 'james',
+        key: 11
+
+    }
+]
+
+
+
+// export default {stats, recents}
