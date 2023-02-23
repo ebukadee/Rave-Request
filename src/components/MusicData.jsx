d948c5da2d2a04e13fa584a3f0e85b2568ad549b
ebukadev <degwu2020@gmail.com>
2023-02-23 13:57:37 +0100

    obrigado

--- src/components/MusicData.jsx
+++ src/components/MusicData.jsx
@@ -1,45 +1,57 @@
-import { useQuery } from 'react-query';
-import axios from 'axios'
-// import {SPOTIFY_API_KEY, API_HOST} from '../../.env'
+import { useQuery } from "react-query";
+import axios from "axios";
 
+const getMusic = () => {
+  const options = {
+    method: "GET",
+    url: "https://spotify23.p.rapidapi.com/search/",
+    params: {
+      q: "dunsin",
+      type: "tracks",
+      offset: "0",
+      limit: "10",
+      numberOfTopResults: "5",
+    },
 
+    headers: {
+      "X-RapidAPI-Key": import.meta.env.VITE_SPOTIFY_API_KEY,
+      "X-RapidAPI-Host": import.meta.env.VITE_API_HOST,
+    },
+  };
+  return axios.request(options);
+};
+const MusicData = () => {
+  const { isLoading, isError, data, error } = useQuery("musicData", getMusic);
+  console.log(data?.data.tracks[0])
 
-
- const  MusicData = () => {
-  const { data, isLoading, error } = useQuery('tracks', async () => {
-    const options = {
-      method: 'GET',
-      url: 'https://spotify23.p.rapidapi.com/search/',
-      params: {
-        q: 'billie',
-        type: 'multi',
-        offset: '0',
-        limit: '10',
-        numberOfTopResults: '5'
-      },
-
-        headers: {
-          'X-RapidAPI-Key': process.env.SPOTIFY_API_KEY,
-          'X-RapidAPI-Host': process.env.API_HOST
-        }
-
+  if (isLoading) {
+    return <p>Loading...</p>;
   }
-  axios.request(options)
-  .then( (response) => {
-    const result = response.data.albums.items
-  result.forEach((item)=>{
-    console.log(item.data.name,item.data.artists.items[0].profile.name)
-  })
-  })
-  .catch( (error) => {
-    console.error(error);
-  });
 
- 
-})
-}
+  if (isError) {
+    return <p>Error: {error.message}</p>;
+  } else {
+    return(
+      // <div>
+      //   {data?.data.tracks.map((track)=>(
+      //     <div key={track.data.id}>
+      //     <h3>{track.data.name}</h3>
 
-export default MusicData
-  
+      //     </div>
+      //   ))}
+      // </div>
+      ""
+    );
+  }
 
+  // return (<div>{
 
+  //   // data.data.albums.items.map((what)=>{
+  //   //   <>
+  //   //   <h3>{JSON.stringify(what)}</h3>
+  //   //   </>
+  //   // })
+  //   }</div>);
+};
+
+export default MusicData;
---
+++ src/components/Preview.jsx
@@ -0,0 +1,12 @@
+const Preview = (search) => {
+  return (
+    <div className="absolute w-[300px] mt-[-] bg-mid h-auto break-words ">
+        <h3>sfdhakssljdhfajslhfkajshlfkajhsfkajsdfhkajshf</h3>
+        <h3>sfdhakssljdhfajslhfkajshlfkajhsfkajsdfhkajshf</h3>
+        <h3>sfdhakssljdhfajslhfkajshlfkajhsfkajsdfhkajshf</h3>   
+
+    </div>
+  );
+};
+
+export default Preview;
