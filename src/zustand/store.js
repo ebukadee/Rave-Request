aafa9068e9446514aed3351b1f45a44837bbd138
ebukadev <degwu2020@gmail.com>
2023-04-29 10:54:54 +0100

    setting up zustand

---
+++ src/zustand/store.js
@@ -0,0 +1,26 @@
+import create from "zustand";
+
+const useStore = create((set) => ({
+  nowPlaying: {
+    image: "https://i.scdn.co/image/ab67616d0000485106013115ced3cd702e75a844",
+    trackName: "Farifax Rain",
+    artist: "Flows of Sleep",
+  },
+  playtime: 0,
+  volume: 50,
+  setNowPlaying: (obj) => {
+    set(() => ({
+      nowPlaying: {
+        image: obj.image,
+        trackName: obj.trackName,
+        artist: obj.artist,
+      },
+    }));
+  },
+  setPlaytime: (num) => {
+    return set(() => ({ playtime: num }));
+  },
+  setVolume: (num) => set(() => ({ volume: num })),
+}));
+
+export default useStore;
