9834c8044fcd3f8b666e54a769e51f585c4780f1
ebukadev <degwu2020@gmail.com>
2023-05-01 20:52:28 +0100

    added preview song to the songs component

--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -4,15 +4,15 @@ export default function Song() {
   const data = useLoaderData();
   const { songId } = useParams();
   return (
-    <section className="w-full h-[100vh] bg-primary">
+    <section className="w-full h-[100vh] bg-primary flex items-center justify-center ">
       <div>
-        <img src={data?.tracks[0].album.images[1].url} alt="Album cover" />
-        <h3>{data?.tracks[0].name}</h3>
-        <h3>{data?.tracks[0].artists[0].name}</h3>
+        <img src={data?.tracks[0]?.album?.images[1]?.url} alt="Album cover" />
+        <h3>{data?.tracks[0]?.name}</h3>
+        <h3>{data?.tracks[0]?.artists[0].name}</h3>
         <audio controls>
-        <source src={data?.tracks[0]} type="audio/mpeg" />
-        Your browser does not support the audio element.
-      </audio>
+          <source src={data?.tracks[0]?.preview_url} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
       </div>
     </section>
   );
