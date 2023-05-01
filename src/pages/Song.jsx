9f3dfa9775ece2c457fa273807b7f4db17116789
ebukadev <degwu2020@gmail.com>
2023-05-01 21:22:58 +0100

    updated the visualiser component

--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,18 +1,18 @@
-import visualiser from "../assets/visualiser.svg"
+import Visualiser from "../components/Visualiser.jsx";
 
 import { useLoaderData, useParams } from "react-router-dom";
 
 export default function Song() {
   const data = useLoaderData();
-  const { songId } = useParams();
   return (
     <section className="w-full h-[100vh] bg-primary flex items-center justify-center ">
       <div>
-          <img
-            src={data?.tracks[0]?.album?.images[1]?.url}
-            alt="Album cover"
-            className="rounded-full"
-          />
+        <img
+          src={data?.tracks[0]?.album?.images[1]?.url}
+          alt="Album cover"
+          className="rounded-full"
+        />
+        <Visualiser />
 
         <h3>{data?.tracks[0]?.name}</h3>
         <h3>{data?.tracks[0]?.artists[0].name}</h3>
