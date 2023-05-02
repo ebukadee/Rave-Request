208e119f146017b71a3af7b8627f068d03b96f40
ebukadev <degwu2020@gmail.com>
2023-05-02 19:56:17 +0100

    styling the audio element

--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -5,7 +5,7 @@ export default function Song() {
   const data = useLoaderData();
   return (
     <section className="w-full h-[100vh] bg-primary  ">
-      <div className=" h-full flex flex-col gap-3 items-center justify-center">
+      <div className=" h-[80%] flex flex-col gap-3 items-center justify-center">
         <img
           src={data?.tracks[0]?.album?.images[1]?.url}
           alt="Album cover"
@@ -17,11 +17,11 @@ export default function Song() {
           <h3>{data?.tracks[0]?.name}</h3>
           <h3>{data?.tracks[0]?.artists[0].name}</h3>
         </div>
-        <audio controls>
-          <source src={data?.tracks[0]?.preview_url} type="audio/mpeg" />
-          Your browser does not support the audio element.
-        </audio>
       </div>
+      <audio controls className="h-[20%] pb-8">
+        <source src={data?.tracks[0]?.preview_url} type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
     </section>
   );
 }
