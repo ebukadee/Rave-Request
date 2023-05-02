bbc95d8de3011d13c91a85f574c5ec8b61c3faf4
ebukadev <degwu2020@gmail.com>
2023-05-02 19:19:25 +0100

    set the layout of the song page

--- src/components/Visualiser.jsx
+++ src/components/Visualiser.jsx
@@ -1,23 +1,219 @@
-import React, { useRef } from 'react';
+// import React, { useRef } from "react";
 
-function MyComponent() {
-  const pathRef = useRef(null);
+export default function Visualiser() {
+  // const pathRef = useRef(null);
 
-  function updatePath() {
-    const path = pathRef.current;
+  // function updatePath() {
+  //   const path = pathRef.current;
 
-    const x1 = 500;
-    const y1 = 100;
-    const x2 = 500;
-    const y2 = 200;
+  //   const x1 = 500;
+  //   const y1 = 100;
+  //   const x2 = 500;
+  //   const y2 = 200;
 
-    path.setAttribute('d', `M${x1} ${y1} L${x2} ${y2}`);
-  }
+  //   path.setAttribute('d', `M${x1} ${y1} L${x2} ${y2}`);
+  // }
 
   return (
-    <svg>
-      <path ref={pathRef} d="M980.878 239.383L980.878 157.783" />
-      <button onClick={updatePath}>Update Path</button>
+    <svg
+      width="822"
+      height="222"
+      viewBox="0 0 1222 422"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M7 211H8.77391"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M1215.04 198.583L1213.26 198.583"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M46.0264 198.583L46.0264 223.417"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M1176.01 211L1176.01 186.165"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M85.0522 198.583L85.0522 223.417"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M1136.98 211L1136.98 186.165"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M124.079 182.617L124.079 239.383"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M1097.96 226.965L1097.96 170.2"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M163.104 182.617L163.104 239.383"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M1058.93 226.965L1058.93 170.2"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M202.131 148.913L202.131 273.087"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M1019.9 260.67L1019.9 136.496"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M241.157 170.2L241.157 251.8"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M980.878 239.383L980.878 157.783"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M280.183 182.617L280.183 239.383"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M941.853 226.965L941.853 170.2"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M319.209 148.913L319.209 273.087"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M902.826 260.67L902.826 136.496"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M358.235 97.4696L358.235 324.53"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M863.8 312.113L863.8 85.0522"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M397.261 138.27L397.261 283.73"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M824.774 271.313L824.774 125.852"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M436.287 170.2L436.287 251.8"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M785.748 239.383L785.748 157.783"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M475.313 182.617L475.313 239.383"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M514.339 148.913L514.339 273.087"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M553.365 97.4696L553.365 324.53"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M592.392 60.2174L592.392 361.783"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M631.417 7L631.417 415"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M670.444 60.2174L670.444 361.783"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M709.47 97.4696L709.47 324.53"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
+      <path
+        d="M748.496 138.27L748.496 283.73"
+        stroke="black"
+        stroke-width="10"
+        stroke-linecap="round"
+      />
     </svg>
   );
 }
--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,12 +1,11 @@
-import Visualiser from "../components/Visualiser.jsx";
-
+import Visualiser from "../components/visualiser";
 import { useLoaderData, useParams } from "react-router-dom";
 
 export default function Song() {
   const data = useLoaderData();
   return (
-    <section className="w-full h-[100vh] bg-primary flex items-center justify-center ">
-      <div>
+    <section className="w-full h-[100vh] bg-primary  ">
+      <div className=" h-full flex flex-col items-center justify-center">
         <img
           src={data?.tracks[0]?.album?.images[1]?.url}
           alt="Album cover"
