c2bc56b06ca0956288c71c69e3d5f50a236fbfd5
ebukadev <degwu2020@gmail.com>
2023-05-05 16:23:34 +0100

    added canvas to draw thee visualiser

--- src/components/Visualiser.jsx
+++ src/components/Visualiser.jsx
@@ -1,21 +1,45 @@
-export default function Visualiser() {
-  const data = [10, 20, 30, 40, 50, 60, 50, 40, 30, 20, 10];
-  const barHeight = 50;
+import { useRef, useEffect } from "react";
 
-  return (
-    <svg width="500" height="300">
-      {data.map((value, index) => (
-        <rect
-          key={index}
-          x={index * 20}
-          y={200 - barHeight}
-          width="10"
-          height={value}
-          rx="5"
-          ry="5"
-          fill="black"
-        />
-      ))}
-    </svg>
-  );
+export default function Visualiser() {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    ctx.fillStyle = "blue";
+    ctx.fillRect(10, 10, 50, 50);
+  }, []);
+
+  class Bar {
+    constructor() {}
+    update() {}
+    draw() {}
+  }
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  return <canvas ref={canvasRef} className="bg-black" />;
+  // const data = [10, 20, 30, 40, 50, 60, 50, 40, 30, 20, 10];
+  // const barHeight = 50;
+
+  // return (
+  //   <svg width="500" height="300">
+  //     {data.map((value, index) => (
+  //       <rect
+  //         key={index}
+  //         x={index * 20}
+  //         y={200 - barHeight}
+  //         width="10"
+  //         height={value}
+  //         rx="5"
+  //         ry="5"
+  //         fill="black"
+  //       />
+  //     ))}
+  //   </svg>
+  // );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Visualiser from "./components/visualiser";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Visualiser />
     {/* <Search/> */}
   </React.StrictMode>
 );
