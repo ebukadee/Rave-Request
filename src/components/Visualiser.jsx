c614f873230120d71cf2cd2883c63d2c70e61a55
ebukadev <degwu2020@gmail.com>
2023-05-05 22:21:04 +0100

    did the canvas stuff

--- src/components/Visualiser.jsx
+++ src/components/Visualiser.jsx
@@ -2,27 +2,47 @@ import { useRef, useEffect } from "react";
 
 export default function Visualiser() {
   const canvasRef = useRef(null);
+  class Bar {
+    constructor(x, y, width, height, color) {
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+      this.color = color;
+    }
+    update(micInput) {
+      // this.height = micInput
+    }
+    draw(context) {
+      context.fillStyle = this.color;
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
+  const bar1 = new Bar(10, 10, 100, 200, "blue");
+
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    function animate() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      bar1.draw(ctx);
+      // console.log(bar1);
+      requestAnimationFrame(animate);
+    }
+    animate();
+  }, [bar1]);
 
-    ctx.fillStyle = "blue";
-    ctx.fillRect(10, 10, 50, 50);
-  }, []);
+  return <canvas ref={canvasRef} />;
 
-  class Bar {
-    constructor() {}
-    update() {}
-    draw() {}
-  }
-  function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  }
+  // const canvasRef = useRef(null);
 
-  return <canvas ref={canvasRef} className="bg-black" />;
+  // function animate() {
+  //   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // }
+
   // const data = [10, 20, 30, 40, 50, 60, 50, 40, 30, 20, 10];
   // const barHeight = 50;
 
